f02ac30f68f626ae62ad3dad5fec424692cdd05b
Charlotte <charlottemaryhailey@gmail.com>
2018-12-06 11:22:26 +0800

    js changes for 'typed'

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
   $(function() {
 
     var $window           = $(window),
-        win_height_padded = $window.height() * 1.1,
+        win_height_padded = $window.height() * 1,
         isTouch           = Modernizr.touch;
 
     if (isTouch) { $('.revealOnScroll').addClass('animated'); }
@@ -46,7 +46,40 @@ $( document ).ready(function() {
     revealOnScroll();
   });
 
-
+  $(function(){
+    $(".typed").typed({
+      strings: ["Developers.", "Designers.", "People."],
+      stringsElement: null,
+      // typing speed
+      typeSpeed: 30,
+      // time before typing starts
+      startDelay: 1200,
+      // backspacing speed
+      backSpeed: 20,
+      // time before backspacing
+      backDelay: 500,
+      // loop
+      loop: true,
+      // false = infinite
+      loopCount: false,
+      // show cursor
+      showCursor: false,
+      // character for cursor
+      cursorChar: "|",
+      // attribute to type (null == text)
+      attr: null,
+      // either html or text
+      contentType: 'html',
+      // call when done callback function
+      callback: function() {},
+      // starting callback function before each string
+      preStringTyped: function() {},
+      //callback for every typed string
+      onStringTyped: function() {},
+      // callback for reset
+      resetCallback: function() {}
+    });
+  });
 
 
 
